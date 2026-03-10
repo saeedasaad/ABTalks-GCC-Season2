bbcce441715308e36433bcc706f44943f0fd0eee
saeedasaad <saeeda23saad@gmail.com>
2026-03-10 20:21:00 +0500

    update folder

---
+++ Day-02/solution.py
@@ -0,0 +1,17 @@
+try:
+
+    nums = list(map(int, input("Enter numbers separated by commas: ").split(",")))
+
+
+    running_sum = []
+    total = 0
+
+
+    for num in nums:
+        total += num        
+        running_sum.append(total)  
+
+    print(f"Running Sum: {running_sum}")
+
+except ValueError:
+    print("Invalid input! Please enter integers only.")
